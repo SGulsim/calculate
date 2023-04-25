cf24a8859bb13e16d13a7a9013e2c87ab4be2dce
Gulsim Saralieva <saralievagulsim@gmail.com>
2023-04-25 23:51:57 +0300

    Html Css for calc

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-let name2 = prompt("Введите имя:", "Гость");
-alert(`Здравствуйте, ${name2}!` );
 
-let firstNum = prompt("Введите первое число");
-let secondNum = prompt("Введите второе число");
+// firstNum = +firstNum;
+// secondNum = +secondNum;
 
-firstNum = +firstNum;
-secondNum = +secondNum;
-
-const sum = firstNum + secondNum;
-const dif = firstNum - secondNum;
-const product = firstNum * secondNum;
-const quo = firstNum / secondNum;
-alert(
-    `Результат:
-    Сумма - ${sum},
-    Разность - ${dif},
-    Произведение - ${product},
-    Деление - ${quo}`
-)
+// const sum = firstNum + secondNum;
+// const dif = firstNum - secondNum;
+// const product = firstNum * secondNum;
+// const quo = firstNum / secondNum;
